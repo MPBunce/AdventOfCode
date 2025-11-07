c2fe0234d2d7df5417c54e6f304c6f018d7ac995
MPBunce <matthew-bunce@outlook.com>
2025-11-07 17:35:15 -0500

    day 1

--- 2024/solution/day_one.go
+++ 2024/solution/day_one.go
@@ -3,16 +3,54 @@ package solution
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 func SolveDayOne(file *os.File) {
+	var left []int
+	var right []int
+
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
+		temp := strings.Fields(line)
+		l, err1 := strconv.Atoi(temp[0])
+		r, err2 := strconv.Atoi(temp[1])
+		if err1 != nil || err2 != nil {
+			fmt.Println("Error converting string to int:", err1, err2)
+			continue
+		}
+		left = append(left, l)
+		right = append(right, r)
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Println("Error reading file:", err)
 	}
+
+	solveDayOnePartOne(left, right)
+	solveDayOnePartTwo(left, right)
+}
+
+func solveDayOnePartOne(left []int, right []int) {
+	sort.Slice(left, func(i, j int) bool {
+		return left[i] < left[j]
+	})
+	sort.Slice(right, func(i, j int) bool {
+		return right[i] < right[j]
+	})
+	var total int
+	for i, _ := range left {
+		diff := left[i] - right[i]
+		abs := int(math.Abs(float64(diff)))
+		total += abs
+	}
+	fmt.Println(total)
+}
+
+func solveDayOnePartTwo(left []int, right []int) {
+
 }
